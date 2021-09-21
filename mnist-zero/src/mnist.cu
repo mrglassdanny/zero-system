59f1007274ce70189c70ad7c84688b632d3dbbd2
d0g0825 <danny.glass0@walmart.com>
2021-09-20 21:13:21 -0500

    got 98%!!!

--- mnist-zero/src/mnist.cu
+++ mnist-zero/src/mnist.cu
@@ -57,15 +57,16 @@ void mnist_test()
 
     Supervisor *sup = init_mnist_supervisor();
 
-    std::vector<int> layer_config = {784, 128, 128, 64, 64, 10};
-    NN *nn = new NN(layer_config, ReLU, ReLU, MSE, 0.1f);
+    // std::vector<int> layer_config = {784, 1024, 1024, 512, 512, 10};
+    // NN *nn = new NN(layer_config, ReLU, ReLU, MSE, 0.1f);
 
-    //NN *nn = new NN("C:\\Users\\d0g0825\\Desktop\\cuda-mnist.nn");
-    //nn->set_learning_rate(0.01f);
+    NN *nn = new NN("C:\\Users\\d0g0825\\Desktop\\temp\\cuda-zero-system-bests\\cuda-mnist.nn");
 
-    nn->all(sup, 100, 100, "C:\\Users\\d0g0825\\Desktop\\mnist-train.csv");
+    //nn->all(sup, 100, 1000, "C:\\Users\\d0g0825\\Desktop\\mnist-train.csv");
 
     //nn->dump("C:\\Users\\d0g0825\\Desktop\\cuda-mnist-2.nn");
+
+    nn->test(sup->create_test_batch()).print();
 
     delete nn;
 
