d05f08622d8259276a7500f884aff5d711de7290
d0g0825 <danny.glass0@walmart.com>
2021-09-22 08:41:44 -0500

    mnist tests

--- mnist-zero/src/mnist.cu
+++ mnist-zero/src/mnist.cu
@@ -57,14 +57,14 @@ void mnist_test()
 
     Supervisor *sup = init_mnist_supervisor();
 
-    std::vector<int> layer_config = {784, 1024, 1024, 512, 512, 256, 128, 64, 10};
-    NN *nn = new NN(layer_config, ReLU, ReLU, MSE, He, 0.05f);
+    std::vector<int> layer_config = {784, 2048, 1024, 512, 256, 128, 64, 32, 10};
+    NN *nn = new NN(layer_config, ReLU, ReLU, MSE, Xavier, 0.05f);
 
-    //nn->all(sup, 100, 1000, "C:\\Users\\d0g0825\\Desktop\\mnist-train-x.csv");
-    nn->all(sup, 100, 1000, "C:\\Users\\d0g0825\\Desktop\\mnist-train-h.csv");
+    nn->all(sup, 1000, 1000, "C:\\Users\\d0g0825\\Desktop\\mnist-train-x.csv");
+    //nn->all(sup, 1000, 1000, "C:\\Users\\d0g0825\\Desktop\\mnist-train-h.csv");
 
-    //nn->dump("C:\\Users\\d0g0825\\Desktop\\cuda-mnist-xavier.nn");
-    nn->dump("C:\\Users\\d0g0825\\Desktop\\cuda-mnist-he.nn");
+    nn->dump("C:\\Users\\d0g0825\\Desktop\\cuda-mnist-xavier.nn");
+    //nn->dump("C:\\Users\\d0g0825\\Desktop\\cuda-mnist-he.nn");
 
     //NN *nn = new NN("C:\\Users\\d0g0825\\Desktop\\temp\\cuda-zero-system-bests\\cuda-mnist.nn");
 
