a9885c0cb0b1850d71ab7f3e80de71b39bddb2d8
d0g0825 <danny.glass0@walmart.com>
2021-09-24 17:27:04 -0500

    option 2 work!

--- chess-zero/src/app.cu
+++ chess-zero/src/app.cu
@@ -11,7 +11,8 @@
 using namespace zero::core;
 using namespace zero::nn;
 
-#define NN_DUMP_PATH "C:\\Users\\d0g0825\\Desktop\\temp\\nn\\chess.nn"
+#define OPT1_NN_DUMP_PATH "C:\\Users\\d0g0825\\Desktop\\temp\\nn\\opt1-chess.nn"
+#define OPT2_NN_DUMP_PATH "C:\\Users\\d0g0825\\Desktop\\temp\\nn\\opt2-chess.nn"
 
 struct DepthSearchResult
 {
@@ -192,7 +193,7 @@ namespace option_1
 
         nn->all(sup, 500, 10000, "C:\\Users\\d0g0825\\Desktop\\temp\\nn\\opt1-chess-train.csv");
 
-        nn->dump(NN_DUMP_PATH, col_cnt);
+        nn->dump(OPT1_NN_DUMP_PATH, col_cnt);
 
         delete nn;
         delete sup;
@@ -471,7 +472,7 @@ namespace option_1
 
     void play_nn(int max_depth)
     {
-        NN *nn = new NN(NN_DUMP_PATH);
+        NN *nn = new NN(OPT1_NN_DUMP_PATH);
 
         int *board = init_board();
         int cpy_board[CHESS_BOARD_LEN] = {0};
@@ -677,6 +678,346 @@ namespace option_2
 
         system("cls");
     }
+
+    void train_nn()
+    {
+        srand(time(NULL));
+
+        FILE *boards_bin_file = fopen("c:\\users\\d0g0825\\desktop\\temp\\chess-zero\\opt2-boards.bs", "rb");
+        FILE *board_labels_bin_file = fopen("c:\\users\\d0g0825\\desktop\\temp\\chess-zero\\opt2-board-labels.bl", "rb");
+
+        long long boards_bin_file_size = get_file_size("c:\\users\\d0g0825\\desktop\\temp\\chess-zero\\opt2-boards.bs");
+        long long board_labels_bin_file_size = get_file_size("c:\\users\\d0g0825\\desktop\\temp\\chess-zero\\opt2-board-labels.bl");
+
+        int col_cnt = CHESS_ONE_HOT_ENCODED_BOARD_LEN * 2;
+        int row_cnt = boards_bin_file_size / (sizeof(float) * col_cnt);
+
+        std::vector<int> layer_cfg = {col_cnt, 2048, 2048, 1024, 256, 64, 1};
+        NN *nn = new NN(layer_cfg, ReLU, ReLU, MSE, Xavier, 0.01f);
+
+        float *data_buf = (float *)malloc(sizeof(float) * (row_cnt * col_cnt));
+        fread(data_buf, sizeof(float) * (row_cnt * col_cnt), 1, boards_bin_file);
+
+        float *lbl_buf = (float *)malloc(sizeof(float) * row_cnt);
+        fread(lbl_buf, sizeof(float) * row_cnt, 1, board_labels_bin_file);
+
+        Supervisor *sup = new Supervisor(row_cnt, col_cnt, 1, data_buf, lbl_buf, Cpu);
+
+        free(data_buf);
+        free(lbl_buf);
+
+        sup->shuffle();
+
+        nn->all(sup, 500, 5000, "C:\\Users\\d0g0825\\Desktop\\temp\\nn\\opt2-chess-train.csv");
+
+        nn->dump(OPT1_NN_DUMP_PATH, col_cnt);
+
+        delete nn;
+        delete sup;
+
+        fclose(boards_bin_file);
+        fclose(board_labels_bin_file);
+    }
+
+    DepthSearchResult depth_search_single(int *board, float agg_eval, int white_mov_flg, NN *nn)
+    {
+        int legal_moves[CHESS_MAX_LEGAL_MOVE_CNT] = {0};
+        char mov[CHESS_MAX_MOVE_LEN] = {0};
+
+        int sim_board[CHESS_BOARD_LEN] = {0};
+
+        int one_hot_encoded_board_pre[CHESS_ONE_HOT_ENCODED_BOARD_LEN] = {0};
+        int one_hot_encoded_board_post[CHESS_ONE_HOT_ENCODED_BOARD_LEN] = {0};
+        int one_hot_encoded_consolidated_board[CHESS_ONE_HOT_ENCODED_BOARD_LEN * 2] = {0};
+
+        float best_eval;
+        char best_mov[CHESS_MAX_MOVE_LEN] = {0};
+
+        if (white_mov_flg == 1)
+        {
+            best_eval = -FLT_MAX;
+        }
+        else
+        {
+            best_eval = FLT_MAX;
+        }
+
+        for (int i = 0; i < CHESS_BOARD_LEN; i++)
+        {
+            if (white_mov_flg == 1)
+            {
+                if (is_piece_white((ChessPiece)board[i]) == 1)
+                {
+                    get_legal_moves(board, i, legal_moves, 1);
+                    for (int j = 0; j < CHESS_MAX_LEGAL_MOVE_CNT; j++)
+                    {
+                        if (legal_moves[j] == CHESS_INVALID_VALUE)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            // Pre-move board state first.
+                            one_hot_encode_board(board, one_hot_encoded_board_pre);
+
+                            // Post-move board state second.
+                            memset(mov, 0, CHESS_MAX_MOVE_LEN);
+                            translate_srcdst_idx_to_mov(board, i, legal_moves[j], mov);
+                            simulate_board_change_w_srcdst_idx(board, i, legal_moves[j], sim_board);
+                            one_hot_encode_board(sim_board, one_hot_encoded_board_post);
+
+                            // Consolidate boards into single board for nn.
+                            memcpy(one_hot_encoded_consolidated_board, one_hot_encoded_board_pre, sizeof(int) * CHESS_ONE_HOT_ENCODED_BOARD_LEN);
+                            memcpy(&one_hot_encoded_consolidated_board[CHESS_ONE_HOT_ENCODED_BOARD_LEN], one_hot_encoded_board_post, sizeof(int) * CHESS_ONE_HOT_ENCODED_BOARD_LEN);
+
+                            Tensor *x = new Tensor(1, CHESS_ONE_HOT_ENCODED_BOARD_LEN * 2, Gpu, one_hot_encoded_consolidated_board);
+                            Tensor *pred = nn->predict(x);
+
+                            float eval = pred->get_idx(0);
+
+                            delete x;
+                            delete pred;
+
+                            if (best_eval < agg_eval + eval)
+                            {
+                                best_eval = agg_eval + eval;
+                                memcpy(best_mov, mov, CHESS_MAX_MOVE_LEN);
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if (is_piece_black((ChessPiece)board[i]) == 1)
+                {
+                    get_legal_moves(board, i, legal_moves, 1);
+                    for (int j = 0; j < CHESS_MAX_LEGAL_MOVE_CNT; j++)
+                    {
+                        if (legal_moves[j] == CHESS_INVALID_VALUE)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            // Pre-move board state first.
+                            one_hot_encode_board(board, one_hot_encoded_board_pre);
+
+                            // Post-move board state second.
+                            memset(mov, 0, CHESS_MAX_MOVE_LEN);
+                            translate_srcdst_idx_to_mov(board, i, legal_moves[j], mov);
+                            simulate_board_change_w_srcdst_idx(board, i, legal_moves[j], sim_board);
+                            one_hot_encode_board(sim_board, one_hot_encoded_board_post);
+
+                            // Consolidate boards into single board for nn.
+                            memcpy(one_hot_encoded_consolidated_board, one_hot_encoded_board_pre, sizeof(int) * CHESS_ONE_HOT_ENCODED_BOARD_LEN);
+                            memcpy(&one_hot_encoded_consolidated_board[CHESS_ONE_HOT_ENCODED_BOARD_LEN], one_hot_encoded_board_post, sizeof(int) * CHESS_ONE_HOT_ENCODED_BOARD_LEN);
+
+                            Tensor *x = new Tensor(1, CHESS_ONE_HOT_ENCODED_BOARD_LEN * 2, Gpu, one_hot_encoded_consolidated_board);
+                            Tensor *pred = nn->predict(x);
+
+                            float eval = pred->get_idx(0);
+
+                            delete x;
+                            delete pred;
+
+                            if (best_eval > agg_eval + eval)
+                            {
+                                best_eval = agg_eval + eval;
+                                memcpy(best_mov, mov, CHESS_MAX_MOVE_LEN);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        DepthSearchResult ds_res;
+        memcpy(ds_res.mov, best_mov, CHESS_MAX_MOVE_LEN);
+        ds_res.agg_eval = best_eval;
+        return ds_res;
+    }
+
+    DepthSearchResult depth_search_recursive(int *immut_sim_board, int white_mov_flg, int white_mov_cur_flg, NN *nn, float agg_eval, int max_depth, int cur_depth, char *prev_mov)
+    {
+        int mut_sim_board[CHESS_BOARD_LEN] = {0};
+        int one_hot_encoded_board[CHESS_ONE_HOT_ENCODED_BOARD_LEN] = {0};
+        int one_hot_encoded_board_pre[CHESS_ONE_HOT_ENCODED_BOARD_LEN] = {0};
+        int one_hot_encoded_board_post[CHESS_ONE_HOT_ENCODED_BOARD_LEN] = {0};
+        int one_hot_encoded_consolidated_board[CHESS_ONE_HOT_ENCODED_BOARD_LEN * 2] = {0};
+
+        int legal_moves[CHESS_MAX_LEGAL_MOVE_CNT] = {0};
+        char mov[CHESS_MAX_MOVE_LEN] = {0};
+
+        float best_eval;
+        char best_mov[CHESS_MAX_MOVE_LEN] = {0};
+
+        if (white_mov_flg == 1)
+        {
+            best_eval = -FLT_MAX;
+        }
+        else
+        {
+            best_eval = FLT_MAX;
+        }
+
+        if (white_mov_flg == 1)
+        {
+            for (int i = 0; i < CHESS_BOARD_LEN; i++)
+            {
+                if (is_piece_white((ChessPiece)immut_sim_board[i]) == 1)
+                {
+                    get_legal_moves(immut_sim_board, i, legal_moves, 1);
+                    for (int j = 0; j < CHESS_MAX_LEGAL_MOVE_CNT; j++)
+                    {
+                        if (legal_moves[j] == CHESS_INVALID_VALUE)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            // Pre-move board state first.
+                            one_hot_encode_board(immut_sim_board, one_hot_encoded_board_pre);
+
+                            // Post-move board state second.
+                            memset(mov, 0, CHESS_MAX_MOVE_LEN);
+                            translate_srcdst_idx_to_mov(immut_sim_board, i, legal_moves[j], mov);
+                            simulate_board_change_w_srcdst_idx(immut_sim_board, i, legal_moves[j], mut_sim_board);
+                            one_hot_encode_board(mut_sim_board, one_hot_encoded_board_post);
+
+                            // Consolidate boards into single board for nn.
+                            memcpy(one_hot_encoded_consolidated_board, one_hot_encoded_board_pre, sizeof(int) * CHESS_ONE_HOT_ENCODED_BOARD_LEN);
+                            memcpy(&one_hot_encoded_consolidated_board[CHESS_ONE_HOT_ENCODED_BOARD_LEN], one_hot_encoded_board_post, sizeof(int) * CHESS_ONE_HOT_ENCODED_BOARD_LEN);
+
+                            Tensor *x = new Tensor(1, CHESS_ONE_HOT_ENCODED_BOARD_LEN, Gpu, one_hot_encoded_board);
+                            Tensor *pred = nn->predict(x);
+
+                            float eval = pred->get_idx(0);
+
+                            printf("MOVE: %s\t%f\n", mov, eval);
+
+                            delete x;
+                            delete pred;
+
+                            if (cur_depth == max_depth)
+                            {
+                                if (best_eval < agg_eval + eval)
+                                {
+                                    best_eval = agg_eval + eval;
+                                    memcpy(best_mov, mov, CHESS_MAX_MOVE_LEN);
+                                }
+                            }
+                            else
+                            {
+                                DepthSearchResult blk_ds_res = depth_search_single(mut_sim_board, agg_eval + eval, !white_mov_flg, nn);
+
+                                if (strlen(blk_ds_res.mov) == 0)
+                                {
+                                    if (best_eval < agg_eval + eval)
+                                    {
+                                        best_eval = agg_eval + eval;
+                                        memcpy(best_mov, mov, CHESS_MAX_MOVE_LEN);
+                                        printf("NO MOVES: %s\n", prev_mov);
+                                    }
+                                }
+                                else
+                                {
+                                    change_board_w_mov(mut_sim_board, blk_ds_res.mov, !white_mov_flg);
+                                    DepthSearchResult rec_ds_res = depth_search_recursive(mut_sim_board, white_mov_flg, white_mov_flg, nn, blk_ds_res.agg_eval, max_depth, cur_depth + 1, mov);
+
+                                    if (best_eval < rec_ds_res.agg_eval)
+                                    {
+                                        best_eval = rec_ds_res.agg_eval;
+                                        memcpy(best_mov, mov, CHESS_MAX_MOVE_LEN);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        else
+        {
+            for (int i = 0; i < CHESS_BOARD_LEN; i++)
+            {
+                if (is_piece_black((ChessPiece)immut_sim_board[i]) == 1)
+                {
+                    get_legal_moves(immut_sim_board, i, legal_moves, 1);
+                    for (int j = 0; j < CHESS_MAX_LEGAL_MOVE_CNT; j++)
+                    {
+                        if (legal_moves[j] == CHESS_INVALID_VALUE)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            // Pre-move board state first.
+                            one_hot_encode_board(immut_sim_board, one_hot_encoded_board_pre);
+
+                            // Post-move board state second.
+                            memset(mov, 0, CHESS_MAX_MOVE_LEN);
+                            translate_srcdst_idx_to_mov(immut_sim_board, i, legal_moves[j], mov);
+                            simulate_board_change_w_srcdst_idx(immut_sim_board, i, legal_moves[j], mut_sim_board);
+                            one_hot_encode_board(mut_sim_board, one_hot_encoded_board_post);
+
+                            // Consolidate boards into single board for nn.
+                            memcpy(one_hot_encoded_consolidated_board, one_hot_encoded_board_pre, sizeof(int) * CHESS_ONE_HOT_ENCODED_BOARD_LEN);
+                            memcpy(&one_hot_encoded_consolidated_board[CHESS_ONE_HOT_ENCODED_BOARD_LEN], one_hot_encoded_board_post, sizeof(int) * CHESS_ONE_HOT_ENCODED_BOARD_LEN);
+
+                            Tensor *x = new Tensor(1, CHESS_ONE_HOT_ENCODED_BOARD_LEN, Gpu, one_hot_encoded_board);
+                            Tensor *pred = nn->predict(x);
+
+                            float eval = pred->get_idx(0);
+
+                            printf("MOVE: %s\t%f\n", mov, eval);
+
+                            delete x;
+                            delete pred;
+
+                            if (cur_depth == max_depth)
+                            {
+                                if (best_eval > agg_eval + eval)
+                                {
+                                    best_eval = agg_eval + eval;
+                                    memcpy(best_mov, mov, CHESS_MAX_MOVE_LEN);
+                                }
+                            }
+                            else
+                            {
+                                DepthSearchResult wht_ds_res = depth_search_single(mut_sim_board, agg_eval + eval, !white_mov_flg, nn);
+                                if (strlen(wht_ds_res.mov) == 0)
+                                {
+                                    if (best_eval > agg_eval + eval)
+                                    {
+                                        best_eval = agg_eval + eval;
+                                        memcpy(best_mov, mov, CHESS_MAX_MOVE_LEN);
+                                        printf("NO MOVES: %s\n", prev_mov);
+                                    }
+                                }
+                                else
+                                {
+                                    change_board_w_mov(mut_sim_board, wht_ds_res.mov, !white_mov_flg);
+                                    DepthSearchResult rec_ds_res = depth_search_recursive(mut_sim_board, white_mov_flg, white_mov_flg, nn, wht_ds_res.agg_eval, max_depth, cur_depth + 1, mov);
+
+                                    if (best_eval > rec_ds_res.agg_eval)
+                                    {
+                                        best_eval = rec_ds_res.agg_eval;
+                                        memcpy(best_mov, mov, CHESS_MAX_MOVE_LEN);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        DepthSearchResult ds_res;
+        memcpy(ds_res.mov, best_mov, CHESS_MAX_MOVE_LEN);
+        ds_res.agg_eval = best_eval;
+        return ds_res;
+    }
 }
 
 int main(int argc, char **argv)
@@ -692,7 +1033,9 @@ int main(int argc, char **argv)
 
     // Option 2:
     {
-        option_2::dump_pgn("c:\\users\\d0g0825\\ml-data\\chess-zero\\Fischer.pgn");
+        //option_2::dump_pgn("c:\\users\\d0g0825\\ml-data\\chess-zero\\Fischer.pgn");
+
+        option_2::train_nn();
     }
 
     return 0;
