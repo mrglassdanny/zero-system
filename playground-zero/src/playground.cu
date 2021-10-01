5867e5e4a377d77c165931bb20d9a506bae112ed
d0g0825 <danny.glass0@walmart.com>
2021-09-30 22:09:26 -0500

    playing

--- playground-zero/src/playground.cu
+++ playground-zero/src/playground.cu
@@ -9,7 +9,7 @@ using namespace zero::cluster;
 
 void nn_test()
 {
-	int x_col_cnt = 384;
+	int x_col_cnt = 26;
 	int y_col_cnt = 1;
 
 	Tensor *x = new Tensor(1, x_col_cnt, Gpu);
@@ -19,8 +19,8 @@ void nn_test()
 	y->set_all(0.0f);
 	y->set_idx(1, 1.0f);
 
-	std::vector<int> layer_config = {x_col_cnt, 512, 256, y_col_cnt};
-	NN *nn = new NN(layer_config, None, None, MSE, 0.01f);
+	std::vector<int> layer_config = {x_col_cnt, 16, 8, 6, 4, y_col_cnt};
+	NN *nn = new NN(layer_config, None, Tanh, MSE, 0.01f);
 
 	nn->check_gradient(x, y, false);
 
@@ -47,23 +47,10 @@ void kmeans_test()
 
 int main(int argc, char **argv)
 {
-	srand(time(NULL));
 
-	//nn_test();
+	nn_test();
 
 	//kmeans_test();
-
-	Tensor *t1 = new Tensor(5, 5, Cpu);
-	Tensor *t2 = new Tensor(5, 5, Cpu);
-
-	t1->set_all_rand_normal_distribution(0.0f, sqrt(2.0f / 2048));
-	t2->set_all_rand(2.0f / sqrt(2048));
-
-	t1->print();
-	t2->print();
-
-	delete t1;
-	delete t2;
 
 	return 0;
 }
