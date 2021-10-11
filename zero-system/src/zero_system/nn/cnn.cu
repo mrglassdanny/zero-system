e3a7b010ba5bb420bb50ea6274b62e64abc725ad
d0g0825 <danny.glass0@walmart.com>
2021-10-11 18:03:18 -0500

    convnet

--- zero-system/src/zero_system/nn/cnn.cu
+++ zero-system/src/zero_system/nn/cnn.cu
@@ -7,4 +7,98 @@ using namespace zero::nn;
 
 // Device functions:
 
+__device__ float d_relu(float val)
+{
+    return val > 0.0f ? val : 0.0f;
+}
+
+__device__ float d_derive_relu(float val)
+{
+    return val > 0.0f ? 1.0f : 0.0f;
+}
+
+__device__ float d_sigmoid(float val)
+{
+    return (1.0 / (1.0 + exp(-val)));
+}
+
+__device__ float d_derive_sigmoid(float val)
+{
+    return (val) * (1.0 - val);
+}
+
+__device__ float d_tanh(float val)
+{
+    return ((exp(val) - exp(-val)) / (exp(val) + exp(-val)));
+}
+
+__device__ float d_derive_tanh(float val)
+{
+    return (1 - (val * val));
+}
+
+__device__ float d_sine(float val)
+{
+    return sin(val);
+}
+
+__device__ float d_derive_sine(float val)
+{
+    return cos(val);
+}
+
+__device__ float d_cosine(float val)
+{
+    return cos(val);
+}
+
+__device__ float d_derive_cosine(float val)
+{
+    return -sin(val);
+}
+
+__device__ float d_mse_cost(float n_val, float y_val)
+{
+    return ((n_val - y_val) * (n_val - y_val));
+}
+
+__device__ float d_derive_mse_cost(float n_val, float y_val)
+{
+    return 2.0f * (n_val - y_val);
+}
+
+__device__ float d_cross_entropy_cost(float n_val, float y_val)
+{
+    return (float)((y_val * log(n_val)) + ((1.0 - y_val) * log(1.0 - n_val)));
+}
+
+__device__ float d_derive_cross_entropy_cost(float n_val, float y_val)
+{
+    return (n_val - y_val);
+}
+
 // Kernel functions:
+
+__global__ void k_cross_correlate(float *x_arr, float *f_arr, float *b_arr, float *y_arr, int x_col_cnt,
+                                  int f_row_cnt, int f_col_cnt)
+{
+    int tid = blockIdx.x * blockDim.x + threadIdx.x;
+
+    int y_row_idx = tid / f_col_cnt;
+    int y_col_idx = tid % f_col_cnt;
+
+    for (int f_row_idx = 0, f_rot_row_idx = f_row_cnt - 1; f_row_idx < f_row_cnt; f_row_idx++, f_rot_row_idx--)
+    {
+        for (int f_col_idx = 0, f_rot_col_idx = f_col_cnt - 1; f_col_idx < f_col_cnt; f_col_idx++, f_rot_col_idx--)
+        {
+            int x_row_idx = y_row_idx + f_row_idx;
+            int x_col_idx = y_col_idx + f_col_idx;
+
+            float val = x_arr[x_row_idx * x_col_cnt + x_col_idx];
+            val *= f_arr[f_rot_row_idx * f_col_cnt + f_rot_col_idx];
+            val += b_arr[y_row_idx * f_col_cnt + y_col_idx];
+
+            y_arr[y_row_idx * f_col_cnt + y_col_idx] += val;
+        }
+    }
+}
--- zero-system/src/zero_system/nn/cnn.cuh
+++ zero-system/src/zero_system/nn/cnn.cuh
@@ -10,9 +10,22 @@ namespace zero
 {
     namespace nn
     {
+        class ConvaLayer
+        {
+        public:
+            std::vector<Tensor *> inputs;
+            std::vector<std::vector<Tensor *>> filters;
+            std::vector<Tensor *> outputs;
+            ActivationFunctionId activation_func_id;
+
+            ConvaLayer();
+            ~ConvaLayer();
+        };
+
         class CNN
         {
         private:
+            std::vector<ConvaLayer> layers;
             NN *nn;
 
         public:
