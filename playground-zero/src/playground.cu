c708ba78249110138c9a7d641593c685537d90f2
d0g0825 <danny.glass0@walmart.com>
2021-10-22 18:35:26 -0500

    conv grad checking

--- playground-zero/src/playground.cu
+++ playground-zero/src/playground.cu
@@ -12,13 +12,16 @@ void v2_test()
 
 	Model *model = new Model(CostFunction::MSE, 0.001f);
 
-	model->add_layer(new LinearLayer(64, 32, InitializationFunction::He));
+	model->add_layer(new ConvolutionalLayer(1, 10, 10, 1, 2, 2, InitializationFunction::He));
+	model->add_layer(new ActivationLayer(81, ActivationFunction::None));
+
+	model->add_layer(new LinearLayer(81, 32, InitializationFunction::He));
 	model->add_layer(new ActivationLayer(32, ActivationFunction::Sigmoid));
-	model->add_layer(new DropoutLayer(32, 0.3f));
+	//model->add_layer(new DropoutLayer(32, 0.3f));
 	model->add_layer(new LinearLayer(32, 8, InitializationFunction::He));
 	model->add_layer(new ActivationLayer(8, ActivationFunction::Sigmoid));
 
-	Tensor *x = new Tensor(Device::Cuda, 64);
+	Tensor *x = new Tensor(Device::Cuda, 100);
 	x->set_all(0.25f);
 	Tensor *y = new Tensor(Device::Cuda, 8);
 	y->set_val(2, 1.0f);
