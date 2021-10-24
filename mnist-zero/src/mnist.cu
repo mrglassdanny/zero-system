7ffdd5482726595e7b2083968426661df5a96df9
d0g0825 <danny.glass0@walmart.com>
2021-10-24 00:44:50 -0500

    mnist testing

--- mnist-zero/src/mnist.cu
+++ mnist-zero/src/mnist.cu
@@ -55,14 +55,17 @@ int main(int argc, char **argv)
 {
     Supervisor *sup = init_mnist_supervisor();
 
-    Model *model = new Model(CostFunction::MSE, 0.00001f);
+    Model *model = new Model(CostFunction::MSE, 0.01f);
 
     std::vector<int> n_shape{1, IMAGE_ROW_CNT, IMAGE_COL_CNT};
 
-    model->add_layer(new ConvolutionalLayer(n_shape, 3, 3, 3, InitializationFunction::He));
+    model->add_layer(new ConvolutionalLayer(n_shape, 32, 3, 3, InitializationFunction::He));
     model->add_layer(new ActivationLayer(model->get_output_shape(), ActivationFunction::ReLU));
 
-    model->add_layer(new ConvolutionalLayer(model->get_output_shape(), 3, 3, 3, InitializationFunction::He));
+    model->add_layer(new ConvolutionalLayer(model->get_output_shape(), 32, 3, 3, InitializationFunction::He));
+    model->add_layer(new ActivationLayer(model->get_output_shape(), ActivationFunction::ReLU));
+
+    model->add_layer(new LinearLayer(model->get_output_shape(), 256, InitializationFunction::He));
     model->add_layer(new ActivationLayer(model->get_output_shape(), ActivationFunction::ReLU));
 
     model->add_layer(new LinearLayer(model->get_output_shape(), 128, InitializationFunction::He));
@@ -71,7 +74,7 @@ int main(int argc, char **argv)
     model->add_layer(new LinearLayer(model->get_output_shape(), Tensor::get_cnt(sup->get_y_shape()), InitializationFunction::He));
     model->add_layer(new ActivationLayer(model->get_output_shape(), ActivationFunction::ReLU));
 
-    model->all(sup, 32, 100, nullptr);
+    model->all(sup, 64, 1000, nullptr);
 
     model->save("C:\\Users\\d0g0825\\Desktop\\temp\\nn\\mnist.nn");
 
