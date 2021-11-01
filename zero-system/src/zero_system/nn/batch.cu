274adad06a92a3fc3225b676079c17ab69ee8b10
d0g0825 <danny.glass0@walmart.com>
2021-11-01 13:52:36 -0500

    sup work

--- zero-system/src/zero_system/nn/batch.cu
+++ zero-system/src/zero_system/nn/batch.cu
@@ -58,14 +58,8 @@ Tensor *Batch::get_y(int idx)
     return this->records[idx]->y;
 }
 
-Supervisor::Supervisor()
+Supervisor::Supervisor(float train_pct, float test_pct)
 {
-}
-
-Supervisor::Supervisor(int row_cnt, int col_cnt, int one_hot_cnt, float *x_arr, float *y_arr, float train_pct, float test_pct, Device device)
-{
-    this->add_all(row_cnt, col_cnt, one_hot_cnt, x_arr, y_arr, device);
-
     if (train_pct + test_pct > 1.0f || train_pct > 1.0f || test_pct > 1.0f)
     {
         this->train_pct = 0.60f;
@@ -82,6 +76,34 @@ Supervisor::Supervisor(int row_cnt, int col_cnt, int one_hot_cnt, float *x_arr, 
 
 Supervisor::~Supervisor()
 {
+}
+
+float Supervisor::get_train_pct()
+{
+    return this->train_pct;
+}
+
+float Supervisor::get_validation_pct()
+{
+    return this->validation_pct;
+}
+
+float Supervisor::get_test_pct()
+{
+    return this->test_pct;
+}
+
+InMemorySupervisor::InMemorySupervisor(float train_pct, float test_pct, int row_cnt, int col_cnt, int one_hot_cnt, float *x_arr, float *y_arr, Device device)
+    : Supervisor(train_pct, test_pct)
+{
+    this->add_all(row_cnt, col_cnt, one_hot_cnt, x_arr, y_arr, device);
+
+    // Go ahead and shuffle!
+    std::random_shuffle(this->records.begin(), this->records.end());
+}
+
+InMemorySupervisor::~InMemorySupervisor()
+{
     for (int i = 0; i < this->records.size(); i++)
     {
         delete this->records[i];
@@ -90,7 +112,7 @@ Supervisor::~Supervisor()
     this->clear();
 }
 
-void Supervisor::add(int col_cnt, int one_hot_cnt, float *x_arr, float y_val, Device device)
+void InMemorySupervisor::add(int col_cnt, int one_hot_cnt, float *x_arr, float y_val, Device device)
 {
     Tensor *x = new Tensor(device, 1, col_cnt);
     x->set_arr(x_arr);
@@ -112,7 +134,7 @@ void Supervisor::add(int col_cnt, int one_hot_cnt, float *x_arr, float y_val, De
     this->records.push_back(new Record(x, y));
 }
 
-void Supervisor::add_all(int row_cnt, int col_cnt, int one_hot_cnt, float *x_arr, float *y_arr, Device device)
+void InMemorySupervisor::add_all(int row_cnt, int col_cnt, int one_hot_cnt, float *x_arr, float *y_arr, Device device)
 {
     this->records.reserve(row_cnt);
 
@@ -122,38 +144,28 @@ void Supervisor::add_all(int row_cnt, int col_cnt, int one_hot_cnt, float *x_arr
     }
 }
 
-void Supervisor::clear()
+void InMemorySupervisor::clear()
 {
     this->records.clear();
 }
 
-int Supervisor::get_cnt()
+int InMemorySupervisor::get_cnt()
 {
     return this->records.size();
 }
 
-float Supervisor::get_train_pct()
-{
-    return this->train_pct;
-}
-
-std::vector<int> Supervisor::get_x_shape()
+std::vector<int> InMemorySupervisor::get_x_shape()
 {
     return this->records[0]->x->get_shape();
 }
 
-std::vector<int> Supervisor::get_y_shape()
+std::vector<int> InMemorySupervisor::get_y_shape()
 {
     return this->records[0]->y->get_shape();
 }
 
-void Supervisor::shuffle()
-{
-    std::random_shuffle(this->records.begin(), this->records.end());
-}
-
 // Creates batch with all data
-Batch *Supervisor::create_batch()
+Batch *InMemorySupervisor::create_batch()
 {
     int cnt = this->get_cnt();
 
@@ -172,7 +184,7 @@ Batch *Supervisor::create_batch()
     return batch;
 }
 
-Batch *Supervisor::create_batch(int lower, int upper)
+Batch *InMemorySupervisor::create_batch(int lower, int upper)
 {
     if (this->get_cnt() == 0)
     {
@@ -190,7 +202,7 @@ Batch *Supervisor::create_batch(int lower, int upper)
 }
 
 // Creates batch of specified size and bounds with random records.
-Batch *Supervisor::create_batch(int batch_size, int lower, int upper)
+Batch *InMemorySupervisor::create_batch(int batch_size, int lower, int upper)
 {
     if (this->get_cnt() == 0)
     {
@@ -208,23 +220,178 @@ Batch *Supervisor::create_batch(int batch_size, int lower, int upper)
     return batch;
 }
 
-Batch *Supervisor::create_train_batch()
+Batch *InMemorySupervisor::create_train_batch()
 {
     return this->create_batch(0, (int)floor(this->records.size() * this->train_pct));
 }
 
-Batch *Supervisor::create_train_batch(int batch_size)
+Batch *InMemorySupervisor::create_train_batch(int batch_size)
 {
     return this->create_batch(batch_size, 0, (int)floor(this->records.size() * this->train_pct));
 }
 
-Batch *Supervisor::create_validation_batch()
+Batch *InMemorySupervisor::create_validation_batch()
 {
     return this->create_batch((int)floor(this->records.size() * this->train_pct), (int)floor(this->records.size() *
                                                                                              (this->train_pct + this->validation_pct)));
 }
 
-Batch *Supervisor::create_test_batch()
+Batch *InMemorySupervisor::create_test_batch()
 {
     return this->create_batch((int)floor(this->records.size() * (this->train_pct + this->validation_pct)), this->records.size());
+}
+
+OnDiskSupervisor::OnDiskSupervisor(float train_pct, float test_pct, const char *x_path, const char *y_path, std::vector<int> x_shape, int y_one_hot_cnt)
+    : Supervisor(train_pct, test_pct)
+{
+    this->x_file_ptr = fopen(x_path, "rb");
+    this->y_file_ptr = fopen(y_path, "rb");
+
+    this->x_file_size = get_file_size(x_path);
+    this->y_file_size = get_file_size(y_path);
+
+    this->x_shape = x_shape;
+    this->y_one_hot_cnt = y_one_hot_cnt;
+
+    if (train_pct + test_pct > 1.0f || train_pct > 1.0f || test_pct > 1.0f)
+    {
+        this->train_pct = 0.60f;
+        this->validation_pct = 0.20f;
+        this->test_pct = 0.20f;
+    }
+    else
+    {
+        this->train_pct = train_pct;
+        this->validation_pct = 1.0f - (train_pct + test_pct);
+        this->test_pct = test_pct;
+    }
+}
+
+OnDiskSupervisor::~OnDiskSupervisor()
+{
+    fclose(this->x_file_ptr);
+    fclose(this->y_file_ptr);
+}
+
+int OnDiskSupervisor::get_cnt()
+{
+    return this->x_file_size / (sizeof(float) * Tensor::get_cnt(this->x_shape));
+}
+
+Batch *OnDiskSupervisor::create_batch(int cnt, int lower, int upper, bool rand_flg)
+{
+    Batch *batch = new Batch(true, cnt);
+
+    int x_shape_cnt = Tensor::get_cnt(this->x_shape);
+
+    int x_record_size = sizeof(float) * x_shape_cnt;
+    int y_record_size = sizeof(float);
+
+    float *x_buf = (float *)malloc(x_record_size);
+    float *y_buf = (float *)malloc(y_record_size);
+
+    int x_lower_offset = lower * x_record_size;
+    int y_lower_offset = lower * y_record_size;
+
+    fseek(this->x_file_ptr, x_lower_offset, SEEK_SET);
+    fseek(this->y_file_ptr, y_lower_offset, SEEK_SET);
+
+    for (int i = 0; i < cnt; i++)
+    {
+        if (rand_flg)
+        {
+            int rand_idx = rand() % (upper - lower) + lower;
+
+            fseek(this->x_file_ptr, rand_idx * x_record_size + x_lower_offset, SEEK_SET);
+            fseek(this->y_file_ptr, rand_idx * y_record_size + y_lower_offset, SEEK_SET);
+        }
+
+        fread(x_buf, sizeof(float), x_shape_cnt, this->x_file_ptr);
+        Tensor *x = new Tensor(Device::Cpu, this->x_shape);
+        x->set_arr(x_buf);
+
+        fread(y_buf, sizeof(float), 1, this->y_file_ptr);
+        Tensor *y;
+        if (this->y_one_hot_cnt > 1)
+        {
+            y = Tensor::one_hot_encode(Device::Cpu, 1, this->y_one_hot_cnt, y_buf);
+        }
+        else
+        {
+            y = new Tensor(Device::Cpu, 1);
+            y->set_arr(y_buf);
+        }
+
+        batch->add(new Record(x, y));
+    }
+
+    free(x_buf);
+    free(y_buf);
+
+    return batch;
+}
+
+// Creates batch with all data
+Batch *OnDiskSupervisor::create_batch()
+{
+    int cnt = this->get_cnt();
+
+    if (cnt == 0)
+    {
+        return nullptr;
+    }
+
+    Batch *batch = this->create_batch(cnt, 0, this->get_cnt(), false);
+
+    return batch;
+}
+
+Batch *OnDiskSupervisor::create_batch(int lower, int upper)
+{
+    if (this->get_cnt() == 0)
+    {
+        return nullptr;
+    }
+
+    Batch *batch = this->create_batch(upper - lower, lower, upper, false);
+
+    return batch;
+}
+
+// Creates batch of specified size and bounds with random records.
+Batch *OnDiskSupervisor::create_batch(int batch_size, int lower, int upper)
+{
+    if (this->get_cnt() == 0)
+    {
+        return nullptr;
+    }
+
+    Batch *batch = this->create_batch(batch_size, lower, upper, true);
+
+    return batch;
+}
+
+Batch *OnDiskSupervisor::create_train_batch()
+{
+    return this->create_batch(0, (int)floor(this->get_cnt() * this->train_pct));
+}
+
+Batch *OnDiskSupervisor::create_train_batch(int batch_size)
+{
+    return this->create_batch(batch_size, 0, (int)floor(this->get_cnt() * this->train_pct));
+}
+
+Batch *OnDiskSupervisor::create_validation_batch()
+{
+    int cnt = this->get_cnt();
+
+    return this->create_batch((int)floor(cnt * this->train_pct), (int)floor(cnt *
+                                                                            (this->train_pct + this->validation_pct)));
+}
+
+Batch *OnDiskSupervisor::create_test_batch()
+{
+    int cnt = this->get_cnt();
+
+    return this->create_batch((int)floor(cnt * (this->train_pct + this->validation_pct)), cnt);
 }
--- zero-system/src/zero_system/nn/batch.cuh
+++ zero-system/src/zero_system/nn/batch.cuh
@@ -45,37 +45,89 @@ namespace zero
 
         class Supervisor
         {
-        private:
-            std::vector<Record *> records; // Supervisor owns records!
+        public:
             float train_pct;
             float validation_pct;
             float test_pct;
 
-        public:
-            Supervisor();
-            Supervisor(int row_cnt, int col_cnt, int one_hot_cnt, float *x_arr, float *y_arr, float train_pct, float test_pct, Device device);
+            Supervisor(float train_pct, float test_pct);
             ~Supervisor();
+
+            float get_train_pct();
+            float get_validation_pct();
+            float get_test_pct();
+
+            virtual int get_cnt() = 0;
+
+            virtual std::vector<int> get_x_shape() = 0;
+            virtual std::vector<int> get_y_shape() = 0;
+
+            virtual Batch *create_batch() = 0;
+            virtual Batch *create_batch(int lower, int upper) = 0;
+            virtual Batch *create_batch(int batch_size, int lower, int upper) = 0;
+            virtual Batch *create_train_batch() = 0;
+            virtual Batch *create_train_batch(int batch_size) = 0;
+            virtual Batch *create_validation_batch() = 0;
+            virtual Batch *create_test_batch() = 0;
+        };
+
+        class InMemorySupervisor : Supervisor
+        {
+        private:
+            std::vector<Record *> records; // InMemorySupervisor owns records!
 
             void add(int col_cnt, int one_hot_cnt, float *x_arr, float y_val, Device device);
             void add_all(int row_cnt, int col_cnt, int one_hot_cnt, float *x_arr, float *y_arr, Device device);
             void clear();
 
-            int get_cnt();
-            float get_train_pct();
+        public:
+            InMemorySupervisor(float train_pct, float test_pct, int row_cnt, int col_cnt, int one_hot_cnt, float *x_arr, float *y_arr, Device device);
+            ~InMemorySupervisor();
 
-            std::vector<int>
-            get_x_shape();
-            std::vector<int> get_y_shape();
+            virtual int get_cnt();
 
-            void shuffle();
+            virtual std::vector<int> get_x_shape();
+            virtual std::vector<int> get_y_shape();
 
-            Batch *create_batch();
-            Batch *create_batch(int lower, int upper);
-            Batch *create_batch(int batch_size, int lower, int upper);
-            Batch *create_train_batch();
-            Batch *create_train_batch(int batch_size);
-            Batch *create_validation_batch();
-            Batch *create_test_batch();
+            virtual Batch *create_batch();
+            virtual Batch *create_batch(int lower, int upper);
+            virtual Batch *create_batch(int batch_size, int lower, int upper);
+            virtual Batch *create_train_batch();
+            virtual Batch *create_train_batch(int batch_size);
+            virtual Batch *create_validation_batch();
+            virtual Batch *create_test_batch();
+        };
+
+        class OnDiskSupervisor : Supervisor
+        {
+        private:
+            FILE *x_file_ptr;
+            FILE *y_file_ptr;
+
+            long long x_file_size;
+            long long y_file_size;
+
+            std::vector<int> x_shape;
+            int y_one_hot_cnt;
+
+            Batch *create_batch(int cnt, int lower, int upper, bool rand_flg);
+
+        public:
+            OnDiskSupervisor(float train_pct, float test_pct, const char *x_path, const char *y_path, std::vector<int> x_shape, int y_one_hot_cnt);
+            ~OnDiskSupervisor();
+
+            virtual int get_cnt();
+
+            virtual std::vector<int> get_x_shape();
+            virtual std::vector<int> get_y_shape();
+
+            virtual Batch *create_batch();
+            virtual Batch *create_batch(int lower, int upper);
+            virtual Batch *create_batch(int batch_size, int lower, int upper);
+            virtual Batch *create_train_batch();
+            virtual Batch *create_train_batch(int batch_size);
+            virtual Batch *create_validation_batch();
+            virtual Batch *create_test_batch();
         };
     }
 }
