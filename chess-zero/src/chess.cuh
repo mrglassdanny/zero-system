6a645e470cf9721c739b5c9e785f9d519ba63e4d
d0g0825 <danny.glass0@walmart.com>
2021-11-04 16:43:43 -0500

    refactor

--- chess-zero/src/chess.cuh
+++ chess-zero/src/chess.cuh
@@ -104,4 +104,4 @@ void one_hot_encode_board(int *board, int *out);
 
 float eval_board(int *board, Model *model, float *cuda_flt_board_buf);
 
-MinimaxEvaluation get_minimax_eval(int *board, bool white_flg, bool cur_white_flg, int max_depth, int cur_depth, Model *model, float cur_worst_eval, float *cuda_flt_board_buf);
+MinimaxEvaluation get_minimax_eval(int *board, bool white_flg, bool cur_white_flg, int max_depth, int cur_depth, Model *model, float cur_best_eval, float *cuda_flt_board_buf);
