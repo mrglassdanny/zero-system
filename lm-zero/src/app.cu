e26f24231ac8d8e8575737e1320f2c06b062cf08
dglass-cellaware <daniel.glass@cellawaretech.com>
2021-11-16 17:35:25 -0600

    stuff

--- lm-zero/src/app.cu
+++ lm-zero/src/app.cu
@@ -8,6 +8,7 @@ using namespace zero::nn;
 
 int main(int argc, char **argv)
 {
+    srand(time(NULL));
 
     Tensor *x = Tensor::from_csv("C:\\Users\\danie\\OneDrive\\Desktop\\palpck-x.csv");
     Tensor *y = Tensor::from_csv("C:\\Users\\danie\\OneDrive\\Desktop\\palpck-y.csv");
@@ -15,14 +16,21 @@ int main(int argc, char **argv)
     InMemorySupervisor *sup = new InMemorySupervisor(0.90f, 0.10f, x->get_shape()[0], x->get_shape()[1],
                                                      0, x->get_arr(), y->get_arr(), Device::Cpu);
 
-    Model *model = new Model(CostFunction::MSE, 0.001f);
+    Model *model = new Model(CostFunction::MSE, 0.01f);
 
     std::vector<int> x_shape{x->get_shape()[1]};
 
-    model->add_layer(new LinearLayer(x_shape, 64, InitializationFunction::Xavier));
-    model->add_layer(new LinearLayer(model->get_output_shape(), 32, InitializationFunction::Xavier));
+    model->add_layer(new LinearLayer(x_shape, 32, InitializationFunction::Xavier));
+    model->add_layer(new ActivationLayer(model->get_output_shape(), ActivationFunction::ReLU));
+
+    model->add_layer(new LinearLayer(model->get_output_shape(), 16, InitializationFunction::Xavier));
+    model->add_layer(new ActivationLayer(model->get_output_shape(), ActivationFunction::ReLU));
+
     model->add_layer(new LinearLayer(model->get_output_shape(), 8, InitializationFunction::Xavier));
+    model->add_layer(new ActivationLayer(model->get_output_shape(), ActivationFunction::ReLU));
+
     model->add_layer(new LinearLayer(model->get_output_shape(), 1, InitializationFunction::Xavier));
+    model->add_layer(new ActivationLayer(model->get_output_shape(), ActivationFunction::ReLU));
 
     model->train_and_test(sup, 100, 5, "C:\\Users\\danie\\OneDrive\\Desktop\\lm-zero-train.csv");
 
