f835a8736c324e584a8816a453e64c230fc63221
mrglassdanny <mrglassdanny@gmail.com>
2021-11-27 16:16:58 -0600

    tweaks prior to playing Brian

--- chess-zero/src/app.cu
+++ chess-zero/src/app.cu
@@ -736,7 +736,7 @@ int main(int argc, char **argv)
 
     //test_chess("test", "C:\\Users\\danny\\Desktop\\chess-zero\\chess-zero.nn");
 
-    play_chess("C:\\Users\\danny\\Desktop\\chess-zero\\chess-zero.nn", true, 3, true);
+    play_chess("C:\\Users\\danny\\Desktop\\chess-zero\\chess-zero.nn", true, 3, false);
 
     return 0;
 }
