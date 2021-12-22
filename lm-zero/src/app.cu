5627decfd608999e41aeb9a2f31f638add5b7bbc
dglass-cellaware <daniel.glass@cellawaretech.com>
2021-12-21 21:56:44 -0600

    locmst clustering work

--- lm-zero/src/app.cu
+++ lm-zero/src/app.cu
@@ -2,13 +2,27 @@
 #include <iostream>
 
 #include <zero_system/nn/model.cuh>
+#include <zero_system/cluster/kmeans.cuh>
 
 using namespace zero::core;
 using namespace zero::nn;
+using namespace zero::cluster;
 
 int main(int argc, char **argv)
 {
     srand(time(NULL));
+
+    Tensor *xs = Tensor::fr_csv("data/locmst.csv");
+
+    KMeans::save_best(xs, 30, 10, "temp/model.km");
+
+    KMeans *km = new KMeans("temp/model.km");
+
+    km->print();
+
+    delete km;
+
+    delete xs;
 
     return 0;
 }
