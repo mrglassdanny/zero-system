23aaba9e7d3af05dee2c636dd7b108a2d9077228
dglass-cellaware <daniel.glass@cellawaretech.com>
2022-02-22 18:37:28 -0600

    testing

--- lm-zero/src/app.cu
+++ lm-zero/src/app.cu
@@ -30,17 +30,17 @@ int main(int argc, char **argv)
     Table *xs_tbl = Table::fr_csv("data/deeplm_data-test.csv");
     Table *ys_tbl = xs_tbl->split("elapsed_secs");
 
-    int x_actcod_emb_idx = xs_tbl->get_column_idx("actcod");
+    xs_tbl->scale_down();
+    ys_tbl->scale_down();
 
     xs_tbl->encode_ordinal("actcod");
     xs_tbl->encode_onehot("typ");
 
+    int x_actcod_emb_idx = xs_tbl->get_column_idx("actcod");
     int x_fr_loc_beg_idx = xs_tbl->get_column_idx("fr_loc_token_1");
     int x_fr_loc_end_idx = xs_tbl->get_column_idx("fr_loc_token_3");
     int x_to_loc_beg_idx = xs_tbl->get_column_idx("to_loc_token_1");
     int x_to_loc_end_idx = xs_tbl->get_column_idx("to_loc_token_3");
-
-    xs_tbl->scale_down();
 
     Tensor *xs = Table::to_tensor(xs_tbl);
     Tensor *ys = Table::to_tensor(ys_tbl);
@@ -56,39 +56,41 @@ int main(int argc, char **argv)
 
     // Model setup:
 
-    EmbeddableModel *m = new EmbeddableModel(MSE, 0.0000000000000000001f);
+    EmbeddableModel *m = new EmbeddableModel(MSE, 0.01f);
 
     Embedding *actcod_emb = new Embedding(x_actcod_emb_idx);
-    actcod_emb->linear(1, 4);
+    actcod_emb->linear(1, 2);
     actcod_emb->activation(Sigmoid);
-    // m->embed(actcod_emb);
+    m->embed(actcod_emb);
 
     Embedding *fr_loc_emb = new Embedding(x_fr_loc_beg_idx, x_fr_loc_end_idx);
-    fr_loc_emb->linear(3, 8);
+    fr_loc_emb->linear(3, 5);
     fr_loc_emb->activation(Sigmoid);
-    // m->embed(fr_loc_emb);
+    m->embed(fr_loc_emb);
 
     Embedding *to_loc_emb = new Embedding(x_to_loc_beg_idx, x_to_loc_end_idx);
-    to_loc_emb->linear(3, 8);
+    to_loc_emb->linear(3, 5);
     to_loc_emb->activation(Sigmoid);
-    // m->embed(to_loc_emb);
+    m->embed(to_loc_emb);
 
-    m->linear(batch->get_x_shape(), 256);
-    m->activation(ReLU);
-
-    m->linear(256);
-    m->activation(ReLU);
+    m->linear(batch->get_x_shape(), 128);
+    m->activation(Sigmoid);
 
     m->linear(32);
-    m->activation(ReLU);
+    m->activation(Sigmoid);
+
+    m->linear(8);
+    m->activation(Sigmoid);
 
     m->linear(Tensor::get_cnt(batch->get_y_shape()));
 
     // Fit:
 
-    m->fit(batch);
+    m->check_grad(batch->get_x(0), batch->get_y(0), true);
 
-    m->test(batch).print();
+    // m->fit(batch);
+
+    // Cleanup:
 
     delete m;
     delete batch;
