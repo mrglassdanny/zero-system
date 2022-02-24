6e091af31574adc0d53581ee21861405849dbad8
dglass-cellaware <daniel.glass@cellawaretech.com>
2022-02-24 15:41:33 -0600

    fmt

--- zero-system/src/zero_system/nn/model.cu
+++ zero-system/src/zero_system/nn/model.cu
@@ -793,7 +793,6 @@ Tensor *EmbeddedModel::forward(Tensor *x, bool train_flg)
 
     // We need to create an embedded x tensor to match our updated shape and values due to embeddings:
     Tensor *embd_x = new Tensor(x->get_device(), this->get_input_shape());
-
     if (this->embeddings.size() > 0)
     {
         // First we need to shift all non-embedding inputs in accordance to new embedded x layout:
