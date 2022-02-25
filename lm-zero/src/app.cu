4c0e02bbb4dedd2d18b9f1e21f1c8472987e6e45
dglass-cellaware <daniel.glass@cellawaretech.com>
2022-02-24 23:30:48 -0600

    testing

--- lm-zero/src/app.cu
+++ lm-zero/src/app.cu
@@ -27,7 +27,7 @@ int main(int argc, char **argv)
 
     // Data setup:
 
-    Table *xs_tbl = Table::fr_csv("data/deeplm_data-test.csv");
+    Table *xs_tbl = Table::fr_csv("data/deeplm_data.csv");
     Table *ys_tbl = xs_tbl->split("elapsed_secs");
 
     xs_tbl->scale_down();
@@ -59,43 +59,37 @@ int main(int argc, char **argv)
 
     // Model setup:
 
-    EmbeddedModel *embd_m = new EmbeddedModel(MSE, 0.1f);
+    EmbeddedModel *embd_m = new EmbeddedModel(MSE, 0.00001f);
 
     Embedding *actcod_embg = new Embedding(x_actcod_idx);
-    actcod_embg->linear(1, 64);
-    actcod_embg->activation(ReLU);
-    actcod_embg->linear(16);
+    actcod_embg->linear(1, 4);
     actcod_embg->activation(ReLU);
     embd_m->embed(actcod_embg);
 
     Embedding *fr_loc_embg = new Embedding(x_fr_loc_beg_idx, x_fr_loc_end_idx);
-    fr_loc_embg->linear(3, 128);
-    fr_loc_embg->activation(ReLU);
-    fr_loc_embg->linear(32);
+    fr_loc_embg->linear(3, 8);
     fr_loc_embg->activation(ReLU);
     embd_m->embed(fr_loc_embg);
 
     Embedding *to_loc_embg = new Embedding(x_to_loc_beg_idx, x_to_loc_end_idx);
-    to_loc_embg->linear(3, 128);
-    to_loc_embg->activation(ReLU);
-    to_loc_embg->linear(32);
+    to_loc_embg->linear(3, 8);
     to_loc_embg->activation(ReLU);
     embd_m->embed(to_loc_embg);
 
-    embd_m->linear(embd_m->get_embedded_input_shape(sup->get_x_shape()), 256);
+    embd_m->linear(embd_m->get_embedded_input_shape(sup->get_x_shape()), 128);
     embd_m->activation(ReLU);
 
-    embd_m->linear(128);
+    embd_m->linear(64);
     embd_m->activation(ReLU);
 
-    embd_m->linear(32);
+    embd_m->linear(16);
     embd_m->activation(ReLU);
 
     embd_m->linear(Tensor::get_cnt(sup->get_y_shape()));
 
     // Fit:
 
-    embd_m->fit(sup, 64, 500, "temp/train.csv");
+    embd_m->fit(sup, 128, 10, "temp/train.csv");
 
     // Cleanup:
 
