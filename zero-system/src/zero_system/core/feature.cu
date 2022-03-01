2f814d55e9e1478091d2250804f441b3d1a81967
dglass-cellaware <daniel.glass@cellawaretech.com>
2022-02-28 18:23:06 -0600

    onehot tweak

--- zero-system/src/zero_system/core/feature.cu
+++ zero-system/src/zero_system/core/feature.cu
@@ -363,7 +363,7 @@ std::vector<Column *> Column::encode_onehot()
     Column *ordinal_col = new Column(this->name, true, row_cnt);
 
     std::map<std::string, int> ordinal_map;
-    int ordinal_id = 1;
+    int ordinal_id = 0;
 
     for (int row_idx = 0; row_idx < this->row_cnt; row_idx++)
     {
