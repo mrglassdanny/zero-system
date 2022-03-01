4c1c181f5271b7ed9434cd02fec11ac1cc97533f
dglass-cellaware <daniel.glass@cellawaretech.com>
2022-03-01 00:20:17 -0600

    testing

--- lm-zero/src/app.cu
+++ lm-zero/src/app.cu
@@ -17,58 +17,13 @@ void upd_rslt_fn(Tensor *p, Tensor *y, int *cnt)
     }
 }
 
-int main(int argc, char **argv)
+void fit_1(Table *xs_tbl, Table *ys_tbl, Supervisor *sup, const char *path)
 {
-    ZERO();
-
-    // Data setup:
-    Supervisor *sup;
-
-    int x_actcod_idx;
-    int x_fr_loc_beg_idx;
-    int x_fr_loc_end_idx;
-    int x_to_loc_beg_idx;
-    int x_to_loc_end_idx;
-    {
-        Table *xs_tbl = Table::fr_csv("data/palpck.csv");
-        Table *ys_tbl = xs_tbl->split("elapsed_secs");
-
-        delete xs_tbl->remove_column("cas_qty");
-        delete xs_tbl->remove_column("cas_len");
-        delete xs_tbl->remove_column("cas_wid");
-        delete xs_tbl->remove_column("cas_hgt");
-        delete xs_tbl->remove_column("cas_wgt");
-        // delete xs_tbl->remove_column("pal_qty");
-
-        xs_tbl->scale_down();
-        ys_tbl->scale_down();
-
-        xs_tbl->encode_ordinal("actcod");
-        xs_tbl->encode_onehot("typ");
-
-        x_actcod_idx = xs_tbl->get_column_idx("actcod");
-        x_fr_loc_beg_idx = xs_tbl->get_column_idx("fr_loc_token_1");
-        x_fr_loc_end_idx = xs_tbl->get_column_idx("fr_loc_token_3");
-        x_to_loc_beg_idx = xs_tbl->get_column_idx("to_loc_token_1");
-        x_to_loc_end_idx = xs_tbl->get_column_idx("to_loc_token_3");
-
-        Tensor *xs = Table::to_tensor(xs_tbl);
-        Tensor *ys = Table::to_tensor(ys_tbl);
-
-        delete xs_tbl;
-        delete ys_tbl;
-
-        Tensor::to_file("temp/xs.tr", xs);
-        Tensor::to_file("temp/ys.tr", ys);
-
-        std::vector<int> x_shape{xs->get_shape()[1]};
-        sup = new Supervisor("temp/xs.tr", "temp/ys.tr", x_shape, 0);
-
-        delete xs;
-        delete ys;
-    }
-
-    // Model setup:
+    int x_actcod_idx = xs_tbl->get_column_idx("actcod");
+    int x_fr_loc_beg_idx = xs_tbl->get_column_idx("fr_loc_token_1");
+    int x_fr_loc_end_idx = xs_tbl->get_column_idx("fr_loc_token_3");
+    int x_to_loc_beg_idx = xs_tbl->get_column_idx("to_loc_token_1");
+    int x_to_loc_end_idx = xs_tbl->get_column_idx("to_loc_token_3");
 
     EmbeddedModel *embd_m = new EmbeddedModel(MSE, 0.01f);
 
@@ -97,7 +52,48 @@ int main(int argc, char **argv)
 
     // Fit:
 
-    embd_m->fit(sup, 100, 10, "temp/train.csv", upd_rslt_fn);
+    embd_m->fit(sup, 100, 10, "temp/train-1.csv", upd_rslt_fn);
+
+    Batch *test_batch = sup->create_batch();
+    embd_m->test(test_batch, upd_rslt_fn).print();
+
+    delete test_batch;
+
+    embd_m->save(path);
+
+    delete embd_m;
+}
+
+void fit_2(Table *xs_tbl, Table *ys_tbl, Supervisor *sup, const char *path)
+{
+    EmbeddedModel *embd_m = new EmbeddedModel(MSE, 0.01f);
+
+    embd_m->linear(embd_m->get_embedded_input_shape(sup->get_x_shape()), 256);
+    embd_m->activation(ReLU);
+    embd_m->linear(64);
+    embd_m->activation(ReLU);
+    embd_m->linear(16);
+    embd_m->activation(ReLU);
+    embd_m->linear(1);
+
+    // Fit:
+
+    embd_m->fit(sup, 100, 10, "temp/train-2.csv", upd_rslt_fn);
+
+    Batch *test_batch = sup->create_batch();
+    embd_m->test(test_batch, upd_rslt_fn).print();
+
+    delete test_batch;
+
+    embd_m->save(path);
+
+    delete embd_m;
+}
+
+void test(Supervisor *sup, Column *pred_col, const char *path)
+{
+    EmbeddedModel *embd_m = new EmbeddedModel();
+    embd_m->load(path);
 
     Batch *test_batch = sup->create_batch();
     embd_m->test(test_batch, upd_rslt_fn).print();
@@ -105,29 +101,74 @@ int main(int argc, char **argv)
     for (int i = 0; i < test_batch->get_size(); i++)
     {
         Tensor *pred = embd_m->forward(test_batch->get_x(i), false);
-        test_batch->get_x(i)->print();
-        test_batch->get_y(i)->print();
-        pred->print();
-
+        pred_col->set_val(i, pred->get_val(0));
         delete pred;
     }
 
     delete test_batch;
 
-    embd_m->save("temp/embd_m.em");
+    delete embd_m;
+}
+
+int main(int argc, char **argv)
+{
+    ZERO();
+
+    // Data setup:
+
+    Table *xs_tbl = Table::fr_csv("data/palpck-w-locs.csv");
+    Table *ys_tbl = xs_tbl->split("elapsed_secs");
+
+    Column *fr_loc_col = xs_tbl->remove_column("fr_loc");
+    Column *to_loc_col = xs_tbl->remove_column("to_loc");
+
+    delete xs_tbl->remove_column("cas_qty");
+    delete xs_tbl->remove_column("cas_len");
+    delete xs_tbl->remove_column("cas_wid");
+    delete xs_tbl->remove_column("cas_hgt");
+    delete xs_tbl->remove_column("cas_wgt");
+
+    xs_tbl->scale_down();
+    ys_tbl->scale_down();
+
+    // xs_tbl->encode_ordinal("actcod");
+    xs_tbl->encode_onehot("actcod");
+    xs_tbl->encode_onehot("typ");
+
+    Tensor *xs = Table::to_tensor(xs_tbl);
+    Tensor *ys = Table::to_tensor(ys_tbl);
+
+    Tensor::to_file("temp/xs.tr", xs);
+    Tensor::to_file("temp/ys.tr", ys);
+
+    std::vector<int> x_shape{xs->get_shape()[1]};
+    Supervisor *sup = new Supervisor("temp/xs.tr", "temp/ys.tr", x_shape, 0);
+
+    delete xs;
+    delete ys;
+
+    // Fit:
+    {
+        // fit_1(xs_tbl, ys_tbl, sup, "temp/embd_m_1.em");
+        fit_2(xs_tbl, ys_tbl, sup, "temp/embd_m_2.em");
+    }
 
     // Test:
+    // {
+    //     Column *y_col = ys_tbl->get_column("elapsed_secs");
+    //     Column *pred_col = new Column("pred", true, xs_tbl->get_row_cnt());
 
-    // EmbeddedModel *embd_m = new EmbeddedModel();
-    // embd_m->load("temp/embd_m.em");
+    //     xs_tbl->add_column(fr_loc_col);
+    //     xs_tbl->add_column(to_loc_col);
+    //     xs_tbl->add_column(y_col);
+    //     xs_tbl->add_column(pred_col);
 
-    // Batch *test_batch = sup->create_batch();
-    // embd_m->test(test_batch, upd_rslt_fn).print();
-    // delete test_batch;
+    //     test(sup, pred_col, "temp/embd_m_2.em");
+
+    //     Table::to_csv("temp/preds-2.csv", xs_tbl);
+    // }
 
     // Cleanup:
-
-    delete embd_m;
 
     delete sup;
 
